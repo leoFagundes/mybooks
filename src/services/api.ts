b8f65c82989fa365e384298e3af058179a3cdee9
leoFagundes <leofagundes2015@gmail.com>
2024-08-06 13:10:06 -0300

    fix errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://mybooks-api-q5v7.onrender.com",
-  // baseURL: "http://localhost:3001",
+  // baseURL: "https://mybooks-api-q5v7.onrender.com",
+  baseURL: "http://localhost:3001",
   headers: {
     Accept: "application/json, text/plain, */*",
     "Content-Type": "application/json",
